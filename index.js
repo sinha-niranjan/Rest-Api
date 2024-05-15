6b8e396998abf5047a6c557237860d50e63cd053
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-05-16 00:53:33 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {review} from "./src/data.js"
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(review);
+});
+
+app.listen( 3000, () => {
+  console.log("Example app listening on port 3000!");
+});
