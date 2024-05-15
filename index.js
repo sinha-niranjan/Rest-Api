84798e7668a059030a5a46acb148ff8145ea383e
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-05-16 01:09:10 +0530

    cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
-import {review} from "./src/data.js"
+import { review } from "./src/data.js";
+import cors from "cors";
 const app = express();
+
+app.use(cors());
 
 app.get("/review", (req, res) => {
   res.send(review);
 });
 
-app.listen( 3000, () => {
+app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
 });
